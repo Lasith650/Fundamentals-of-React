9dd0ccbb0f816c21bcd29d77e1babc028542ef53
lasith650 <lasith650@gmail.com>
2019-05-10 00:24:20 +0530

    Adding parameterized destructuring to Greet.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-//import Greet from './components/Greet';
+import Greet from './components/Greet';
 //import Welcome from './components/Welcome';
 //import Message from './components/Message';
-import Count from './components/Count';
+//import Count from './components/Count';
 
 function App() {
   return (
@@ -14,12 +14,12 @@ function App() {
        </Greet>
        <Greet name="Manaram" heroName="Amma">
         <button>Submit</button>
-       </Greet>
+       </Greet> */}
        <Greet name="Jayalath" heroName="White"/>
-       <Welcome name="Kasun"/>
-       <Welcome name="Perera"/> */}
+       {/* <Welcome name="Kasun"/> */}
+       {/* <Welcome name="Perera"/> */}
        {/* <Message/> */}
-       <Count/>
+       {/* <Count/> */}
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Greet =(props) => {
+const Greet =({name, heroName}) => {
     return (
     <div>
-    <h1>Hello {props.name} your hero is {props.heroName}</h1>
-    {props.children}
+    <h1>Hello {name} your hero is {heroName}</h1>
+    {/* {props.children} */}
     </div>
     )
 }
