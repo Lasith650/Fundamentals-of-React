c982d2ecdce53b2bc9dc1ae21d7069c630b2f903
lasith650 <lasith650@gmail.com>
2019-05-09 22:23:38 +0530

    Adding previous status to Count.js

--- src/components/Count.js
+++ src/components/Count.js
@@ -9,20 +9,23 @@ class Count extends Component {
     }
 
     increment () {
-        this.setState ({
-            count: this.state.count + 1
-        },
-        () => {
-            console.log('Callback value', this.state.count)
-            }
-        )
-        console.log(this.state.count)
+        this.setState (prevState => ({
+            count: prevState.count + 1
+        }))
+    }
+
+    incrementFive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
   render() {
     return (
       <div>
         <h1>count - { this.state.count}</h1>
-        <button onClick = {() => this.increment()}>Increment</button>
+        <button onClick = {() => this.incrementFive()}>Increment</button>
       </div>
     )
   }
