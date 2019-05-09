e7d5d3f4d3a97ec93b7ba2ecf74e82e63b5389f1
lasith650 <lasith650@gmail.com>
2019-05-10 00:38:44 +0530

    Adding destructuring to Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-//import Welcome from './components/Welcome';
+//import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 //import Message from './components/Message';
 //import Count from './components/Count';
 
@@ -15,9 +15,9 @@ function App() {
        <Greet name="Manaram" heroName="Amma">
         <button>Submit</button>
        </Greet> */}
-       <Greet name="Jayalath" heroName="White"/>
+       {/* <Greet name="Jayalath" heroName="White"/> */}
        {/* <Welcome name="Kasun"/> */}
-       {/* <Welcome name="Perera"/> */}
+       <Welcome name="Perera"/>
        {/* <Message/> */}
        {/* <Count/> */}
     </div>
