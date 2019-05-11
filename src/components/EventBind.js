df87512ab4ea146cd89cb241342434578d98e158
lasith650 <lasith650@gmail.com>
2019-05-12 00:19:19 +0530

    Adding binding in the render method

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react'
 
 class EventBind extends Component{
 
+    constructor(props){
+        super(props)
+        this.state = {
+            message: 'Hi'
+        }
+    }
+
+    clickHandler() {
+        this.setState({
+            message: 'Good Bye my dear friend'
+        })
+    }
+
     render(){
         return(
-            <button>Click</button>
+            <div>
+                <h3>{this.state.message}</h3>
+                <button onClick={this.clickHandler.bind(this)}>Click</button>
+            </div>
+            
         )
     }
 }
