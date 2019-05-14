82b97fbc2b0b9d5c9612b74e50636c3b623e1b97
lasith650 <lasith650@gmail.com>
2019-05-14 21:58:49 +0530

    Adding template literals to ParentComponent.js

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,21 @@
+import react, { component } from 'react'
+
+class ParentComponent extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            parentName: 'Parent'
+        }
+    }
+
+    greetParent(){
+        alert(`Hello ${this.state.parentName}`)
+    }
+    render() {
+        return(
+            <div></div>
+        )
+    }
+
+}
